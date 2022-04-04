4c6716cb625ce5e99873efbab2b2f5136dc8395c
TheAykac <farukomeraykac@gmail.com>
2022-04-04 20:22:49 +0300

    HRMS Swaggaer
    Hrms Projeme swagger desteği getirildi.

---
+++ src/main/java/com/example/hrms/business/abstracts/PositionService.java
@@ -0,0 +1,10 @@
+package com.example.hrms.business.abstracts;
+
+import java.util.List;
+
+import com.example.hrms.entities.concretes.Position;
+
+public interface PositionService {
+	List<Position> getAll();
+
+}
